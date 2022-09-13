d47fbe01adf3309b033a70f51ac1da27d9fc9ac8
Kayk magalhães <leesunga0@gmail.com>
2022-09-13 08:04:46 -0300

    Atividade 10
    Atividade sobre Object

---
+++ Atividades JS/Atividade 10/atv10.js
@@ -0,0 +1,13 @@
+let apartment = {
+    portas:3,
+    banheiro:2,
+    quartos:3,
+    cozinha:{
+        quantidade:1,
+        fogao:1,
+        guardaLouca:1
+    }
+}
+
+console.log(apartment);
+console.log(apartment.cozinha.quantidade)
